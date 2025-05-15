c2983f8f0870b9fbdf820c7cc54f6997cb821c53
Vincent Daubry <vdaubry@gmail.com>
2025-05-15 15:07:19 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # Create a dummy user for testing
 user = User.create!(
   email: 'vdaubry@gmail.com',
@@ -7,7 +8,32 @@ user = User.create!(
   token: SecureRandom.hex(16),
   refresh_token: SecureRandom.hex(16),
   language: 'fr',
-  admin: false
+  admin: true
+)
+puts "Created test user with email: #{user.email}" 
+
+user = User.create!(
+  email: 'lea.juskiewenski@fresqueduclimat.org',
+  password: 'Azerty123',
+  firstname: 'Lea',
+  lastname: 'Juskiewenski',
+  token: SecureRandom.hex(16),
+  refresh_token: SecureRandom.hex(16),
+  language: 'fr',
+  admin: true
+)
+
+puts "Created test user with email: #{user.email}" 
+
+user = User.create!(
+  email: 'bastien.grandjean@fresqueduclimat.org',
+  password: 'Azerty123',
+  firstname: 'Bastien',
+  lastname: 'Grandjean',
+  token: SecureRandom.hex(16),
+  refresh_token: SecureRandom.hex(16),
+  language: 'fr',
+  admin: true
 )
 
 puts "Created test user with email: #{user.email}" 
