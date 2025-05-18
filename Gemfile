2175fa879c9a99906480bb52bf41cd7e6f65c4d3
Vincent Daubry <vdaubry@gmail.com>
2025-05-18 17:08:20 +0200

    update openfresk

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.2"
 
-gem 'open_fresk', :github => 'https://github.com/fresqueduclimat/OpenFresk', :branch => 'feature/administrate-v2'
+gem 'open_fresk', :github => 'https://github.com/fresqueduclimat/OpenFresk', :branch => 'main'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.8", ">= 7.0.8.1"
--- app/models/smtp_setting.rb
+++ app/models/smtp_setting.rb
@@ -1,5 +1,14 @@
 class SmtpSetting < ApplicationRecord
-  attr_encrypted :password, key: ENV['ATTR_ENCRYPTED_KEY']
+  ENCRYPTION_KEY_HEX = ENV['ATTR_ENCRYPTED_KEY']
+
+  if ENCRYPTION_KEY_HEX.nil? || ENCRYPTION_KEY_HEX.length != 64
+    raise "ATTR_ENCRYPTED_KEY environment variable not set or not a 64-character hex string."
+  end
+
+  # Decode the hex key into raw bytes for attr_encrypted
+  DECODED_KEY = [ENCRYPTION_KEY_HEX].pack('H*').freeze
+
+  attr_encrypted :password, key: DECODED_KEY
   validates :domain, :password, :user_name, presence: true
   validates :port, presence: true, numericality: { only_integer: true }
 end
