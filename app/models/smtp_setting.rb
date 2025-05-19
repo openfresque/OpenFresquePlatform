5b5cad73a2caee2a68cef5893d9f7e0d1ea0f3b5
Vincent Daubry <vdaubry@gmail.com>
2025-05-19 06:26:29 +0200

    add comments on bug fix

--- app/models/smtp_setting.rb
+++ app/models/smtp_setting.rb
@@ -1,5 +1,12 @@
 class SmtpSetting < ApplicationRecord
-  attr_encrypted :password, key: [ENV['ATTR_ENCRYPTED_KEY']].pack('H*')
+  attr_encrypted :password, key: :encryption_key
   validates :domain, :password, :user_name, presence: true
   validates :port, presence: true, numericality: { only_integer: true }
+
+  private
+
+  def encryption_key
+    # Convert the 64-character hex string to a 32-byte binary key required by the AES256 encryption algorithm
+    [ENV['ATTR_ENCRYPTED_KEY']].pack('H*') 
+  end
 end
