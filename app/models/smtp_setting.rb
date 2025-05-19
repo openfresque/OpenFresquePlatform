b5a879d6e8041ea17fa7e73538fe6f4b1a221182
Vincent Daubry <vdaubry@gmail.com>
2025-05-19 06:19:31 +0200

    fix attr_encrypted

--- app/models/smtp_setting.rb
+++ app/models/smtp_setting.rb
@@ -1,5 +1,5 @@
 class SmtpSetting < ApplicationRecord
-  attr_encrypted :password, key: ENV['ATTR_ENCRYPTED_KEY']
+  attr_encrypted :password, key: [ENV['ATTR_ENCRYPTED_KEY']].pack('H*')
   validates :domain, :password, :user_name, presence: true
   validates :port, presence: true, numericality: { only_integer: true }
 end
